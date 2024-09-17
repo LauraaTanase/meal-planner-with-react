ce756bda8b9e6cfaca7ea2cc3344467158a94eff
LauraaTanase <zaraflaura@gmail.com>
2024-09-17 14:11:28 -0500

    SearchBarComponent modified

--- src/components/SearchBarComponent.js
+++ src/components/SearchBarComponent.js
@@ -1,17 +1,17 @@
 import React from 'react';
 
-const FooterComponent = () => {
+const SearchBarComponent = ({ searchValue, onSearchChange }) => {
   return (
-    <footer className="bg-dark text-white text-center py-3 fixed-bottom">
-      <div className="container-fluid">
-        <p className="mb-0">&copy; 2024 Recipe and Meal Planning. All rights reserved.</p>
-        <p className="mb-0">
-          <a href="/privacy" className="text-white">Privacy Policy</a> | 
-          <a href="/terms" className="text-white ms-2">Terms and Conditions</a>
-        </p>
-      </div>
-    </footer>
+    <div className="mb-3">
+      <input
+        type="text"
+        className="form-control"
+        placeholder="🔎Search for recipes"
+        value={searchValue}
+        onChange={(a) => onSearchChange(a.target.value)}
+      />
+    </div>
   );
 };
 
-export default FooterComponent;
+export default SearchBarComponent;
