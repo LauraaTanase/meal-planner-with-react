a62e8ba41f03344f821d5271134f3c6510236f59
LauraaTanase <zaraflaura@gmail.com>
2024-09-18 07:51:44 -0500

    HeaderComponent is good to use

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -3,19 +3,19 @@ import { Link } from 'react-router-dom';
 
 const HeaderComponent = () => {
   const headerStyle = {
-    backgroundColor: '#004526', /* Schimbă culoarea de fundal */
-    color: '#fff', /* Schimbă culoarea textului */
+    backgroundColor: '#004526', 
+    color: '#fff', 
     textAlign: 'center',
     padding: '1rem',
   };
 
   const navbarStyle = {
-    backgroundColor: '#001a00', /* Schimb culoarea de fundal a navbar-ului */
+    backgroundColor: '#001a00', 
   };
 
   const navLinkStyle = {
-    color: '#fff', /* Schimbă culoarea textului link-urilor */
-    textDecoration: 'none', /* Elimină sublinierea link-urilor */
+    color: '#fff', 
+    textDecoration: 'none', 
   };
 
   return (
