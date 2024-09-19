856815b3dd28a67d3a5874fb0be66e5f98777536
LauraaTanase <zaraflaura@gmail.com>
2024-09-19 09:40:09 -0500

    Adding new classes to RecipeDetailPage

--- src/pages/RecipeDetailPage.js
+++ src/pages/RecipeDetailPage.js
@@ -7,14 +7,12 @@ import FooterComponent from "../components/FooterComponent";
 const RecipeDetailPage = () => {
   const { id } = useParams();
   return (
-    <div className="container-fluid">
+    <div className="d-flex flex-column min-vh-100">
       <HeaderComponent />
-      <div className="py-5">
-        <div className="container">
-          <RecipeDetailPageContainer />
-        </div>
-        <FooterComponent />
+      <div className="container flex-grow-1 py-5">
+        <RecipeDetailPageContainer />
       </div>
+      <FooterComponent className="mt-auto" />
     </div>
   );
 };
